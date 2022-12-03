97f4ac484f9988c9aa904bf0804e29cf394e6fad
Kenny Adetiloye <kehinde.adetiloye@gmail.com>
2022-12-03 13:51:51 -0500

    Revamp code

--- gatewayserver/src/main/java/com/kenny/forum/gateway/GatewayServerApplication.java
+++ gatewayserver/src/main/java/com/kenny/forum/gateway/GatewayServerApplication.java
@@ -2,10 +2,7 @@ package com.kenny.forum.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
