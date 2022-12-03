28c444d6f84a5f645e3a07bc97aef24ca496df2f
Kenny Adetiloye <kehinde.adetiloye@gmail.com>
2022-12-03 12:15:52 -0500

    Add gateway server

---
+++ gatewayserver/src/main/java/com/sysspecial/forum/gateway/GatewayServerApplication.java
@@ -0,0 +1,37 @@
+package com.kenny.forum.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/kennz/posts/**").filters(f -> f.rewritePath("/kennz/posts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://POSTS"))
+				.build();
+	}
+
+//	private Buildable<Route> swaggerUi(PredicateSpec p, String service, String expectedValue) {
+//		return p.path("/swagger-ui/index.html").and().query("configUrl", expectedValue)
+//				.uri(service);
+//
+//	}
+}
